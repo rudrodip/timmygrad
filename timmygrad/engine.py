2827ba9597ebd4f4d3832fc047e4b9a2bdd4c9d0
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-02 20:43:18 +0600

    added log function

--- timmygrad/engine.py
+++ timmygrad/engine.py
@@ -50,6 +50,16 @@ class Value:
     out._backward = _backward
     return out
   
+  def log(self):
+    x = self.data
+    out = Value(math.log(x), (self,), 'log')
+
+    def _backward():
+      self.grad += (1/x) * out.grad
+
+    out._backward = _backward
+    return out
+  
   def relu(self):
     out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
 
